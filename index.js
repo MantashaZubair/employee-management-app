3600732fadafec73a4a6816f710204a0b712ed24
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2024-09-25 15:40:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,26 +15,7 @@ const corsOptions = {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     credentials: true,
   };
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         const allowedOrigins = ["http://localhost:5173", "https://employee-management-app-wnce.onrender.com"];
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     optionsSuccessStatus: 200,
-//     credentials: true,
-// };
-// const corsOptions = {
-//     origin: [
-//         "http://localhost:5173", // your local development server
-//         "https://employee-management-app-wnce.onrender.com" // your deployed frontend
-//     ],
-//     optionsSuccessStatus: 200,
-//     credentials: true,
-// };
+
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "./client/dist")))
