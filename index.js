da84e0431ac57f8f1eb8921011411c2572150534
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2024-09-25 13:00:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const categoryRoutes= require("./routes/category.routes")
 const app = express()
 connectDB()
 const corsOptions = {
-    origin: "https://employee-management-app-wnce.onrender.com",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     credentials: true,
   };
